c8ffb8eef6ed4a76e02faaf2f52ba05db7f47794
Joe Pasquantonio <joe.pasquantonio@gmail.com>
2019-01-26 10:56:04 -0500

    remove 'index' page to not cause issues with reverse url lookup

--- crypto/urls.py
+++ crypto/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('marketdata.urls')),
     path('marketdata/', include('marketdata.urls')),
     path('admin/', admin.site.urls),
 ]
--- marketdata/urls.py
+++ marketdata/urls.py
@@ -6,5 +6,5 @@ app_name='marketdata'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('/currency/<int:pk>/', views.CurrencyView.as_view(), name='currency'),
+    path('currency/<int:pk>/', views.CurrencyView.as_view(), name='currency'),
 ]
